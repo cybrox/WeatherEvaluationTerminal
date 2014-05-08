9a8a8d365fd7d8af1004421ea83ddfba05a87a91
Sven Marc Gehring <marc@cybrox.eu>
2014-05-08 15:06:18 +0200

    added notification handler

--- api/core/notification-handler.class.php
+++ api/core/notification-handler.class.php
@@ -11,6 +11,21 @@
 
   class NotificationHandler {
     
+    private $db;
+
+    public function __construct(){
+      $this->db = new DatabaseHandler();
+    }
+
+    public function readNotification(){
+      return $this->db->findRead('notifications', 'seen', false);
+    }
+
+    public function writeNotification($params){
+      if(!preg_match('#[0-9]{1,}#', $params[2])) return array(false, 'invalid notification id');
+      else return $this->db->findWrite('notifications', 'id', $params[2], 'seen', true);
+    }
+
   }
 
 ?>
