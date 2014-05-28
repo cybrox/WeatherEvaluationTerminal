ce11077a57a34bb49a681035c7ab2c0498b46b7f
Sven Marc Gehring <marc@cybrox.eu>
2014-05-28 13:33:29 +0200

    Restructured weather controller

--- assets/compiled/application.js
+++ assets/compiled/application.js
@@ -179,10 +179,6 @@
         avgHm += day.humidity;
       }
       days = dataTm.length;
-      this.set('avgTm', (avgTm / days).toFixed(2));
-      this.set('avgRv', (avgRv / days).toFixed(2));
-      this.set('avgWs', (avgWs / days).toFixed(2));
-      this.set('avgHm', (avgHm / days).toFixed(2));
       graphdataOne = [
         {
           strokeColor: "rgba(28,134,238,1)",
@@ -214,9 +210,19 @@
           data: dataWd
         }
       ];
-      this.set('datasetOne', graphdataOne);
-      this.set('datasetTwo', graphdataTwo);
-      return this.set('datasetDir', graphdataDir);
+      return this.setProperties({
+        datasetMonth: {
+          one: graphdataOne,
+          two: graphdataTwo,
+          dir: graphdataDir
+        },
+        average: {
+          tm: (avgTm / days).toFixed(2),
+          rv: (avgRv / days).toFixed(2),
+          ws: (avgWs / days).toFixed(2),
+          hm: (avgHm / days).toFixed(2)
+        }
+      });
     },
     currentMonth: (function() {
       var monthList;
